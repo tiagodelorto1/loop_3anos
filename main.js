7cb017dfa58583bd5c7b7dc6ca82962a7b1773eb
Tiago Delorto <107191725+tiagodelorto1@users.noreply.github.com>
2024-04-19 08:46:01 -0300

    teste

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
+    var respostaTime = prompt("Quanto é 4x2?");
 
     
-    if (respostaTime.toLowerCase() === "Athlético PR") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "8") {
+      alert("Isso mesmo!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
